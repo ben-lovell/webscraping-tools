9028e6c5e701732995adad4cfa9320dfc4d55b16
ben-lovell <blovell017@gmail.com>
2017-06-16 15:08:52 -0700

    Update html_parsing_tools.py

--- Webscraping_Tools/html_parsing_tools.py
+++ Webscraping_Tools/html_parsing_tools.py
@@ -47,7 +47,7 @@ def clean_and_lowercase_string(list_of_strings, key_word_end):
             if item == key_word_end:
                 break
             else:
-                if (item.isdigit() == True or item[0].isdigit()) and ":" not in item:
+                if item.isdigit() == True and ":" not in item:
                     full_list_of_words.append(float(item))
                 else:
                     full_list_of_words.append(item)
@@ -72,7 +72,7 @@ def get_visible_text_after_keyword_from_html(html, key_word_start, key_word_end)
 
     return clean_and_lowercase_string(visible_text.split(), key_word_end)
 
-def organize_visible_text_from_html(list_of_visible_data, list_of_metrics, key_feature_first_metric):
+def organize_visible_text_from_html(list_of_visible_data, list_of_metrics, date_iterations, key_feature_first_metric, date_counter):
 
     # group the rows together
     grouped_list_of_rows = []
@@ -89,6 +89,7 @@ def organize_visible_text_from_html(list_of_visible_data, list_of_metrics, key_f
     cleaned_list = []
     single_clean_row = []
     for row in grouped_list_of_rows:
+        row.insert(0, date_iterations[date_counter])
         for metric in xrange(len(list_of_metrics)):
             try:
                 single_clean_row.append(row[metric])
@@ -99,14 +100,12 @@ def organize_visible_text_from_html(list_of_visible_data, list_of_metrics, key_f
             #        single_clean_row[len(single_clean_row)] += row[clean_metrics.index(metric) + value]
             #else:
 
-
-
         cleaned_list.append(single_clean_row)
         single_clean_row = []
 
     return cleaned_list
 
-def turn_list_into_csv_file(list_to_write_to_csv, headers, resulting_file_location, resulting_csv_file_name):
+def turn_list_into_csv_file(list_to_write_to_csv, headers, date_iterations, resulting_file_location, resulting_csv_file_name):
     with open(resulting_file_location + resulting_csv_file_name + ".csv",'wb') as f:
         writer= csv.writer(f)
         clean_headers = []
@@ -118,15 +117,15 @@ def turn_list_into_csv_file(list_to_write_to_csv, headers, resulting_file_locati
                 writer.writerow(row)
     f.close()
 
-def turn_html_data_into_csv_file(html_data, key_word_start, key_word_end, metrics, key_feature_first_metric, resulting_file_location, resulting_csv_file_name):
+def turn_html_data_into_csv_file(html_data, key_word_start, key_word_end, metrics, date_iterations, key_feature_first_metric, resulting_file_location, resulting_csv_file_name):
     if len(html_data) > 1:
         end_list = []
         for group in html_data:
             visible_text_from_html = get_visible_text_after_keyword_from_html(group, key_word_start, key_word_end)
-            list_to_write_to_csv = organize_visible_text_from_html(visible_text_from_html, metrics, key_feature_first_metric)
+            list_to_write_to_csv = organize_visible_text_from_html(visible_text_from_html, metrics, date_iterations, key_feature_first_metric, html_data.index(group))
             end_list.append(list_to_write_to_csv)
-        return turn_list_into_csv_file(end_list, metrics, resulting_file_location, resulting_csv_file_name)
+        return turn_list_into_csv_file(end_list, metrics, date_iterations, resulting_file_location, resulting_csv_file_name)
     else:
         visible_text_from_html = get_visible_text_after_keyword_from_html(html_data, key_word_start, key_word_end)
-        list_to_write_to_csv = organize_visible_text_from_html(visible_text_from_html, metrics, key_feature_first_metric)
+        list_to_write_to_csv = organize_visible_text_from_html(visible_text_from_html, metrics, date_iterations, key_feature_first_metric)
         return turn_list_into_csv_file(list_to_write_to_csv, metrics, resulting_file_location, resulting_csv_file_name)
